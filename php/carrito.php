2f61f4142915b26f63d700b8ff87a9405a4a465f
gojwkdjksgusguvksgvuskvguskvsk <castillostiven638@gmail.com>
2024-11-22 11:02:50 -0500

    Create carrito.php

---
+++ php/carrito.php
@@ -0,0 +1,30 @@
+[
+    {
+    "id": 1,
+    "producto": "helado con trozos de chocalate",
+    "descripcion": "delecioso helado con trozos de chocalete que endulsara tu paladar.",
+    "imagen": "helado .jpg",
+    "precio": 2000
+    },
+    {
+    "id": 2,
+    "producto": "helado de fresa",
+    "descripcion": "delicioso helado elaborado con los cultivos de nuestra region.",
+    "imagen": "helado de fresa.jpg",
+    "precio": 2000
+    },
+    {
+    "id": 3,
+    "producto": "helado de vainilla",
+    "descripcion": "delicioso helado de vainilla para tu paradar.",
+    "imagen": "helado de vainilla.jpg",
+    "precio": 2000
+    }, 
+    {
+    "id": 3,
+    "producto": "helado de mora",
+    "descripcion": "delicioso helado de mora artesanal de la region.",
+    "imagen": "helado de mora.jpg",
+    "precio": 2000
+    }   
+   ]
